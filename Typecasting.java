503a0cc1fd55cf4bf3776fc9dc4e33cbfac9b0b1
ankkol2011 <ankkol2011@gmail.com>
2023-02-14 10:56:11 +0530

    initial commit

---
+++ Typecasting.java
@@ -0,0 +1,15 @@
+public class Typecasting
+{
+     public static void main(String[] args) {
+     
+        // float a= 10;
+        // int b = 20;
+        // float c = a*b;
+
+        //  c =  (int) (a * b);
+      int c=-9999999;
+       System.out.println(c);
+
+
+    }
+}
---
+++ takinginput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class takinginput {
+    public static void main(String[] args)
+    {
+        System.out.println("Enter a number\n");
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        System.out.println("the iput value "+n);
+    }
+    
+}
